023f5b3b85126c5bdbe4eed7e7b3b032138414d7
Mike Solomon <msolomon@thrillist.com>
2016-11-17 15:27:02 -0500

    send messages

--- hipchat/hipchat.js
+++ hipchat/hipchat.js
@@ -1,3 +1,5 @@
+const request = require('request');
+
 const HIPCHAT_AUTH_TOKEN = (process.env.HIPCHAT_AUTH_TOKEN) ?
   (process.env.HIPCHAT_AUTH_TOKEN) :
   config.get('hipchatAuthToken');
@@ -19,6 +21,15 @@ var functions = {
       "message_format": "text"
     }
     let url = `https://thrillistmediagroup.hipchat.com/v2/room/${room}/notification?auth_token=${HIPCHAT_AUTH_TOKEN}`
+    request.post(
+      url,
+      { json: message },
+      function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          console.log(body)
+        }
+      }
+    );
   }
 }
 
